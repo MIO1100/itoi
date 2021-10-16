6f3847fe69a4d8e3bb90cecb3f41de99d3b27ff9
mio <qwe@qwe.com>
2021-10-16 09:51:28 +0700

    init

--- main.cpp
+++ main.cpp
@@ -5,11 +5,13 @@
 #include "test/Lab2.h"
 #include "test/Lab3.h"
 #include "test/Lab4.h"
+#include "test/Lab5.h"
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
 //    Lab2::test();
 //    Lab3::test();
-  Lab4::test();
+//    Lab4::test();
+    Lab5::test();
     return 0;
 }
